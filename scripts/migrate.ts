59eb9c77265a92bace211b99e5eb7b07a2fe9421
Ion Utale <utale.ion@gmail.com>
2025-12-21 07:21:11 +0100

    refactor: remove redundant environment variable checks in migration script

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -6,14 +6,6 @@ import pg from 'pg';
 const { Pool } = pg;
 
 async function main() {
-    console.log('Checking environment variables...');
-    console.log('DATABASE_URL is ' + (process.env.DATABASE_URL ? 'set' : 'not set'));
-    if (process.env.DATABASE_URL) {
-        console.log('DATABASE_URL length:', process.env.DATABASE_URL.length);
-    } else {
-        console.log('Available env vars:', Object.keys(process.env));
-    }
-
     const connectionString = process.env.DATABASE_URL;
     if (!connectionString) {
         throw new Error('DATABASE_URL is not set');
